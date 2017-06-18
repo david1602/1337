4fcfac314def35a038567388f04f65743b7a1afb
David Dernovoj <david@dernovoj.com>
2017-06-18 15:05:36 +0100

    Updated the utils with a getRandom function

--- utils.js
+++ utils.js
@@ -1,10 +1,5 @@
 module.exports = {
-    renderStats(stats) {
-        return ['Todays stats:'].concat(
-            Object.keys(stats)
-            .map(key => stats[key])
-            .map(obj => `${obj.first_name}: ${obj.counter}`)
-        )
-        .join('\n');
+    getRandom(min = 0, max = 999) {
+        return Math.floor( Math.random() * (max - min + 1) + min );
     }
 }
