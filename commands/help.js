8f1c846c3a9ad964676d08bc0c61454ea83f5570
David Dernovoj <david@dernovoj.com>
2017-06-18 18:44:35 +0100

    Added the help command

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+// Registers the flameadd command
+module.exports = (bot, ctx) => {
+     bot.onText(/\/help/, (msg, match) => {
+         bot.sendMessage(msg.chat.id, `Anything in <> is a user input. The following commands are available:
+/say <text>
+/stats
+/flameadd <flame of any length>
+/flameadd [<person name, case sensitive>] <flame of any length>
+/say <text of any length>
+/respond /<valid regex>/ <response of any length>`)
+     });
+};
