943f4ff63cdc9f48b713d00a8cf5ae11b48dc0cc
David Dernovoj <david@dernovoj.com>
2017-06-18 15:57:20 +0100

    Added init and error handler to utils

--- utils.js
+++ utils.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+const db = require('db');
+
 module.exports = {
     getRandom(min = 0, max = 999) {
         return Math.floor( Math.random() * (max - min + 1) + min );
+    },
+
+    errHandler(err) {
+        fs.appendFileSync('err.log', err);
+    },
+
+    init(ctx) {
+        return Promise.all([
+            db.flames.getAll(),
+            db.stats.getAll(),
+            db.users.getAll()
+        ])
+        .then( ([flames, stats, users]) => {
+            ctx.users = users;
+            ctx.stats = stats;
+            ctx.flames = flames;
+        });
     }
 }
