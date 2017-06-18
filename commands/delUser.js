c54dd40a230b9fa0263bfb6e9416955f2f739a22
David Dernovoj <david@dernovoj.com>
2017-06-18 21:09:22 +0100

    Added a validation for the users when they are attempted to be deleted

--- commands/delUser.js
+++ commands/delUser.js
@@ -4,6 +4,10 @@ const {errHandler} = require('../utils');
 // Registers the flameadd command
 module.exports = (bot, ctx) => {
      bot.onText(/\/delUser \[.+\]/, (msg, match) => {
+         if (!ctx.users.find(u => u.name === match[1])) {
+             bot.sendMessage(msg.chat.id, 'Who\'s that?.');
+             return;
+         }
          return del(match[1])
             .then(res => bot.sendMessage(msg.chat.id, res));
      });
