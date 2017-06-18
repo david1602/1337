4bf6e03e0fe5ac31e71ad9d07153977cd222697d
David Dernovoj <david@dernovoj.com>
2017-06-18 15:57:42 +0100

    Finished flameadd command

--- commands/flameadd.js
+++ commands/flameadd.js
@@ -1,25 +1,42 @@
+const {create} = require('../db').flames;
+
 // Registers the flameadd command
- module.exports = bot => {
+ module.exports = (bot, ctx) => {
      bot.onText(/\/flameadd (.*)/, (msg, match) => {
          const chatId = msg.chat.id;
          const params = match[1]; // the captured "whatever"
 
-         const tokens = params.split(' ');
+         const userMatch = match[1].match(/\[(.+)\] (.*)/);
 
          const flame = {
              person: null,
              flame: ''
          };
 
-         if (tokens[0].match(/\[.*\]/))
-             flame.person = tokens[0].replace('[', '').replace(']', '');
+         if (userMatch) {
+             flame.person = userMatch[1];
+             flame.flame = userMatch[2];
+         }
+         else
+            flame.flame = match[1];
 
-         flame.person ? flame.flame = tokens.slice(1).join(' ') : flame.flame = tokens.join(' ');
+        if (flame.person && !ctx.users.find(usr => usr.name === flame.person)) {
+            bot.sendMessage(chatId, `Who is ${flame.person}?`)
+            return;
+        }
 
-         const forPerson = flame.person ? ` for ${flame.person}` : '';
-         const resp = `Added flame "${flame.flame}"${forPerson}.`
+        return create(flame.person, flame.flame)
+            .then( () => {
+                const forPerson = flame.person ? ` for ${flame.person}` : '';
+                const resp = `Added flame "${flame.flame}"${forPerson}.`
 
-         // send back the matched "whatever" to the chat
-         bot.sendMessage(chatId, resp);
+                // send back the matched "whatever" to the chat
+                bot.sendMessage(chatId, resp);
+            })
+            .catch(err => {
+                errHandler(err);
+                bot.sendMessage(chatId, 'Failed to add flame, see the logs for more detail.')
+            })
+
      });
  }
