6f7a1dc30089d4d71b4e80faf8b8b2e02e0ce5d2
David Dernovoj <david@dernovoj.com>
2017-06-18 13:04:40 +0100

    Added create and delete functions for flames

--- db.js
+++ db.js
@@ -1,3 +1,67 @@
-module.exports = {
+const pgp = require('pg-promise')();
+const fs = require('fs');
+const path = require('path');
 
+if (!fs.existsSync('config.js')) {
+    throw new Error(`The configuration file does not exist. Refer to the README.`);
+}
+
+const {connection} = require('./config').dbConn;
+const conn = pgp(connection);
+
+module.exports = {
+    flames: {
+
+        /**
+         * Creates a new flame.
+         *
+         * @param  {type} name    Name of the user the flame is for (optional)
+         * @param  {type} content description
+         * @return {type}         description
+         */
+        create(name, content) {
+            return db.none(`
+                INSERT INTO flames(user_id, content)
+                VALUES ((SELECT id FROM users WHERE name = $1), $3)
+            `, [name, content]);
+        },
+
+        delete(content) {
+            return db.any(`
+                SELECT *
+                FROM flames f
+                    INNER JOIN users u ON f.user_id = u.id
+                WHERE content = $1
+            `, [content])
+            .then(data => {
+                const msg = data.map(flame => {
+                    if (flame.user_id)
+                        return `Deleted flame "${flame.content}" for user [${flame.name}]`;
+
+                    return `Deleted flame ${flame.content}`;
+                })
+                .join('\n');
+
+
+                return db.none(`
+                    DELETE FROM flames WHERE content = $1;
+                `)
+                .then( () => msg );
+            })
+        },
+
+        get(id = null) {
+
+        }
+    },
+
+    stats: {
+        create() {
+
+        },
+
+        get() {
+
+        }
+    }
 };
