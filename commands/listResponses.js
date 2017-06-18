0e5bee872b3bab7aa7a58b0317839cb6656c24d0
David Dernovoj <david@dernovoj.com>
2017-06-18 19:00:16 +0100

    Fixed syntax issues in the listResponses command

--- commands/listResponses.js
+++ commands/listResponses.js
@@ -4,12 +4,12 @@ module.exports = (bot, ctx) => {
         const chatId = msg.chat.id;
         const {responses} = ctx;
 
-        if (responses.legnth === 0) {
+        if (responses.length === 0) {
             bot.sendMessage(chatId, 'I don\'t respond to anything yet. Feel free to teach me something. Learn more via the /help command.');
             return;
         }
 
-        const msg = responses.map(resp => `[ID: ${resp.id} | regex: /${resp.regex}/ | response: "${resp.response}"]`);
-        bot.sendMessage(chatId, msg);
+        const chatmsg = responses.map(resp => `[ID: ${resp.id} | regex: /${resp.regex}/ | response: "${resp.response}"]`);
+        bot.sendMessage(chatId, chatmsg);
      });
 };
