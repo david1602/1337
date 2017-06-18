a3181ba5632b9d7953c6e24a8ad2f125e0a98e7f
David Dernovoj <david@dernovoj.com>
2017-06-18 18:46:44 +0100

    removed unused commands

--- commands/coal.js
+++
@@ -1,6 +0,0 @@
-module.exports = bot => {
-    bot.onText(/\/coal/, (msg) => {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, 'Burn the coal, pay the toll.');
-    });
-}
--- commands/dankemerkel.js
+++
@@ -1,6 +0,0 @@
-module.exports = bot => {
-    bot.onText(/\/dankemerkel/, (msg) => {
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, 'djunkrmjurkl');
-    });
-}
