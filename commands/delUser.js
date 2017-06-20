54976d325f004e6f09775c30560df83b2fc334bf
David Dernovoj <david@dernovoj.com>
2017-06-20 12:50:20 +0100

    Added the ID check to the deluser command

--- commands/delUser.js
+++ commands/delUser.js
@@ -1,11 +1,16 @@
 const {del} = require('../db').users;
 const {errHandler} = require('../utils');
+const {ownerid} = require('../config');
 
 // Registers the flameadd command
 module.exports = (bot, ctx) => {
      bot.onText(/\/delUser \[(.+)\]/, (msg, match) => {
          if (!ctx.users.find(u => u.name === match[1])) {
              bot.sendMessage(msg.chat.id, 'Who\'s that?');
+             return;
+         }
+         if (ownerid !== msg.from.id) {
+             bot.sendMessage(msg.chat.id, 'You are not my owner.');
              return;
          }
          return del(match[1])
