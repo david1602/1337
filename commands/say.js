7590ed937c84722ab137f26327630468b60139fb
David Dernovoj <david@dernovoj.com>
2017-06-20 13:01:38 +0100

    Added multiline matching for the say command

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,6 @@
 // Registers the say command
 module.exports = bot => {
-    bot.onText(/\/say (.+)/, (msg, match) => {
+    bot.onText(/\/say ([.\s\S]+)/, (msg, match) => {
         const chatId = msg.chat.id;
         const resp = match[1];
         bot.sendMessage(chatId, resp);
