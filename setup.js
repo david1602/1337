36a443928cf28e53b3deb163758c1577b2982320
David Dernovoj <david@dernovoj.com>
2017-09-24 22:49:54 +0100

    Prettified setup.js

--- setup.js
+++ setup.js
@@ -3,17 +3,22 @@ const fs = require('fs');
 const path = require('path');
 
 if (!fs.existsSync('config.js')) {
-    throw new Error(`The configuration file does not exist. Refer to the README.`);
+    throw new Error(
+        `The configuration file does not exist. Refer to the README.`
+    );
 }
 
-const {connection} = require('./config').dbConn;
+const { connection } = require('./config').dbConn;
 const conn = pgp(connection);
 
 console.log('Setting up database');
 
-const setupSQL = fs.readFileSync(path.resolve('sql', 'setup.sql'), {encoding: 'utf-8'});
+const setupSQL = fs.readFileSync(path.resolve('sql', 'setup.sql'), {
+    encoding: 'utf-8'
+});
 
-conn.none(setupSQL)
-.then( () => console.log('Finished setting up the database') )
-.then( pgp.end )
-.catch( err => console.error('An error occured:', err) );
+conn
+    .none(setupSQL)
+    .then(() => console.log('Finished setting up the database'))
+    .then(pgp.end)
+    .catch(err => console.error('An error occured:', err));
