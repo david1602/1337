684f5179a240bb747004410c4f24c532ee8e3012
David Dernovoj <david@dernovoj.com>
2017-09-24 22:47:49 +0100

    Implemented the new table rendering in the stats command

--- commands/stats.js
+++ commands/stats.js
@@ -1,43 +1,31 @@
 const db = require('../db');
 const utils = require('../utils');
+const Canvas = require('canvas');
 
 module.exports = bot => {
     bot.onText(/\/stats/, msg => {
-      const chatId = msg.chat.id;
+        const chatId = msg.chat.id;
 
-      return db.stats.getStatistics().then(msg => {
-          const html = utils.generateHTMLTable(
-              {
-                  name: {
-                      title: 'Name',
-                      type: 'string'
-                  },
-                  amountposts: {
-                      title: 'Amount Posts',
-                      type: 'numeric'
-                  },
-                  maxstreak: {
-                      title: 'Max Streak',
-                      type: 'numeric'
-                  },
-                  streak: {
-                      title: 'Streak',
-                      type: 'numeric'
-                  }
-              },
-              msg
-          );
+        return db.stats.getStatistics().then(results => {
+            const c = new Canvas();
+            const ctx = c.getContext('2d');
+            ctx.font = '60px Arial';
+            ctx.fillStyle = '#41aff4';
 
-          const renderStream = utils.screenshotHtml(html.html, html.window);
+            const buf = utils.renderTable(
+                {
+                    name: 'Name',
+                    amountposts: 'Amount Posts',
+                    maxstreak: 'Max Streak',
+                    streak: 'Current Streak'
+                },
+                results,
+                ctx
+            );
 
-            return utils.convertStreamToBuffer(renderStream).then( buffer => {
-              bot.sendPhoto(
-                  chatId,
-                  buffer,
-                  {caption: 'Current stats until today:'}
-              );
-          });
-      } );
-
+            return bot.sendPhoto(chatId, buf, {
+                caption: 'Current stats until today'
+            });
+        });
     });
-}
+};
